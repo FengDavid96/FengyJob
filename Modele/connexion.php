0a0da3ad972fd1f750b27d53854879be0be0fc60
FengDavid96 <fyhdavid@gmail.com>
2018-03-07 09:55:55 +0100

    start

---
+++ Modele/connexion.php
@@ -0,0 +1,28 @@
+<?php
+
+try{
+    $dbname = "bdd_tipso";
+    $host = 'localhost';
+    $user = 'root';
+    $passwd = '';
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$passwd");
+}
+
+catch(PDOException $e)
+{
+    try {
+        $dbname = "bdd_tipso";
+        $host = 'localhost';
+        $user = 'root';
+        $passwd = 'root';
+        $bdd = new PDO("mysql:host=$host;dbname=$dbname", "$user", "$passwd");
+        }
+    catch(PDOException $e)
+    {
+        print "Erreur !" . $e->getMessage() . "<br/>";
+        die();
+    }
+}
+$bdd->query("SET NAMES UTF8");
+
+?>
